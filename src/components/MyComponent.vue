ad0190bdca592b2b1efdc9848c12ffc815af8feb
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2020-10-04 23:16:24 +0300

    Bug

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,39 @@
+<template>
+ <button @click="onClick">{{text}}</button>
+</template>
+
+<script>
+
+const baseComponent = {
+  data() {
+    const h = { d: 1 };
+    return{
+      a: h
+    }
+  },
+  updated() {
+    console.log(this.a.d)
+  },
+  provide() {
+      return {
+        a: this.a
+      }
+  },
+};
+
+export default {
+  name: 'HelloWorld',
+  extends: baseComponent,
+  data() {
+    return{
+      text: "text"
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.text = "text2";
+    }
+  }
+}
+</script>
